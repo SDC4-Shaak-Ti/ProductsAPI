6761e1ed3843c85b42e4201b65f2deb91cc537e4
Daniel Elliott <daniel.s.elliott.33@gmail.com>
2022-09-17 08:35:16 -0500

    finished api routes

--- database/index.js
+++ database/index.js
@@ -1,29 +1,93 @@
-require('dotenv').config();
-const {Pool, Client} = require('pg');
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+const {Pool} = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  database: 'products',
-  password: 'password',
-  port: 5432,
+host: process.env.HOST,
+user: process.env.USERNAME,
+database: process.env.DATABASE,
+password: process.env.PASSWORD,
+port: process.env.DATABASE_PORT,
 });
 
-pool.connect()
-  .then(() => {
-    console.log('Successfully connected to database');
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+//get all info for one product
+const getOneProduct = (product_id) => {
+  return pool
+    .connect()
+    .then(client => {
+      return client.query(`SELECT * FROM all_products WHERE id = ${product_id}`)
+      .then(results => {
+        client.release();
+        return results.rows[0]
+      })
+      .catch(err => {
+        client.release();
+        console.log(err);
+      })
+    })
+    .finally(() => {
+      pool.end();
+    });
+}
+// getOneProduct(1);
 
-pool.query('SELECT * FROM all_products WHERE id < 20')
-  .then((results) => {
-    console.log(results);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .then(() => {
-    pool.end()
+//get related products for a product
+const getRelatedProducts = (product_id) => {
+  return pool
+  .connect()
+  .then(client => {
+    // console.log(product_id)
+    return client.query(`SELECT json_agg(related_product_id) FROM related WHERE current_product_id = ${product_id};`)
+    .then(results => {
+      client.release();
+      return results.rows[0].json_agg;
+    })
+    .catch(err => {
+      client.release();
+      console.log(err);
+    })
   });
+}
+// getRelatedProducts(1);
+
+// get all styles for one product
+
+// const getStyles = (product_id) => {
+//   return pool
+//     .connect()
+//     .then(client => {
+//       return client.query(`select json_build_object(
+//         'id', a.id,
+//         'name', a.name,
+//         'slogan', a.slogan,
+//         'description', a.description,
+//         'category', a.category,
+//         'default_price', a.default_price,
+//         'features', (
+//           select json_agg(json_build_object(
+//             'feature', f.feature,
+//             'value', f.value
+//           ))
+//           from features f where f.product_id = a.id
+//         )
+//       ) as result
+//       from all_products a
+//       where a.id = ${product_id};`)
+//       .then(results => {
+//         client.release();
+//         return results.rows[0];
+//       })
+//       .catch(err => {
+//         client.release();
+//         console.log(err);
+//       })
+//     });
+// }
+
+// getStyles(1);
+
+module.exports = {
+  getOneProduct:  getOneProduct,
+  getRelatedProducts:  getRelatedProducts
+  // getStyles:  getStyles
+};
---
+++ server/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const {getOneProduct, getRelatedProducts, /*getStyles*/} = require('/Users/daniel/ProductsAPI/database/index.js');
+
+//middleware
+// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
+//routes
+
+//get related products
+app.get('/products/*/related', (req, res) => {
+  getRelatedProducts(req.params['0'])
+  .then((relatedProducts) => {
+    res.send(relatedProducts);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+//get all features for a product
+
+// app.get('/products/*/styles', (req, res) => {
+//   console.log(req.params)
+//   getStyles(req.params['0'])
+//   .then((productStyles) => {
+//     res.send(productStyles);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// });
+
+//get all product info for one product
+
+app.get('/products/*/', (req, res) => {
+  getOneProduct(req.params['0'])
+    .then((product) => {
+      res.send(product);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`listening on port ${process.env.SERVER_PORT}`);
+});
